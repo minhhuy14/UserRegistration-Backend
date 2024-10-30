4d26e78bf6f1e1ee1fa28fc355f42cda89d06a72
minhhuy14 <minhhuy14012003@gmail.com>
2024-10-30 17:11:22 +0700

    Allow CORS from frontend

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -41,7 +41,7 @@ namespace UserRegistration_Backend.Controllers
 
         // POST: api/Users
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
+        [HttpPost("register")]
         public async Task<ActionResult<User>> PostUser(User user)
         {
             if (user.Email == null || user.Password == null)
--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using UserRegistration_Backend.Data;
+
+string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:5173",
+                              "http://localhost:5173/signup").AllowAnyHeader()
+                                .AllowAnyMethod();
+                      });
+});
+
 builder.Services.AddDbContext<UserRegistration_BackendContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("UserRegistration_BackendContext") ?? throw new InvalidOperationException("Connection string 'UserRegistration_BackendContext' not found.")));
 
@@ -22,6 +35,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
