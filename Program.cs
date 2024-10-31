286e667b6ff7810a3c2fcdde2d90439072f68412
minhhuy14 <minhhuy14012003@gmail.com>
2024-10-31 16:57:43 +0700

    Complete deploy

--- Program.cs
+++ Program.cs
@@ -9,8 +9,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://localhost:5173",
-                              "http://localhost:5173/signup").AllowAnyHeader()
+                          policy.WithOrigins("https://user-registration-frontend-rose.vercel.app").AllowAnyHeader()
                                 .AllowAnyMethod();
                       });
 });
